0198ef9b3d4468c377555a3d4ca751d03093ccd3
Natali993 <manat3017@gmail.com>
2025-09-16 19:19:17 +0300

    fix: title text size for mobile

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const CustomTitle = ({ children }: any) => {
   return (
     <div className="flex justify-center w-full my-8">
-      <h1 className="text-center font-serif uppercase tracking-[0.06em] leading-none text-[#133553] text-[42px] md:text-[56px] lg:text-[64px]">
+      <h1 className="text-center font-serif uppercase tracking-[0.06em] leading-none text-[#133553] text-3xl md:text-[56px] lg:text-[64px]">
         {children}
       </h1>
     </div>
