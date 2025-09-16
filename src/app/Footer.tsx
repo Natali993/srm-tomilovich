fc03eb3bce9e9ca8fcce828cd8f9ad64d27c79ee
Natali993 <manat3017@gmail.com>
2025-09-16 20:18:33 +0300

    fix: footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -6,7 +6,7 @@ const Contacts = () => {
       <h4 className="font-semibold mb-2">Контакти</h4>
       <p>м. Рівне, вул Київська, 42</p>
       <p>+38 (067) 360 75 20</p>
-      <a href="mailto:techprokat.rv.ua@gmail.com" className="text-blue-600 hover:underline">techprokat.rv.ua@gmail.com</a>
+      <a href="mailto:techprokat.rv.ua@gmail.com" className="text-white hover:underline">techprokat.rv.ua@gmail.com</a>
     </div>
   );
 };
@@ -15,8 +15,21 @@ const Logo = () => {
   return (
     <div className="w-full md:w-1/3 flex justify-center items-center">
       <Image
-        src="/techprokat_logo.png"
+        src="/logo-footer.png"
         alt="Логотип Технік"
+        width={256}
+        height={128}
+        className="w-64 h-auto" />
+    </div>
+  );
+};
+
+const LogoGraphicRoboty = () => {
+  return (
+    <div className="w-full md:w-1/3 flex justify-center items-center">
+      <Image
+        src="/grafik-robotu.png"
+        alt="Grafic roboty"
         width={256}
         height={128}
         className="w-64 h-auto" />
@@ -29,10 +42,10 @@ const CategoriesMenu = () => {
       <div className="w-full md:w-1/3">
           <h4 className="font-semibold mb-2">Категорії</h4>
           <ul className="space-y-2 list-disc list-inside">
-            <li><a href="https://techprokat.rv.ua/" className="text-blue-600 hover:underline">Головна</a></li>
-            <li><a href="https://techprokat.rv.ua/pro-nas/" className="text-blue-600 hover:underline">Про нас</a></li>
-            <li><a href="https://techprokat.rv.ua/prokat/" className="text-blue-600 hover:underline">Прокат</a></li>
-            <li><a href="https://techprokat.rv.ua/posluhy/" className="text-blue-600 hover:underline">Послуги</a></li>
+            <li><a href="https://techprokat.rv.ua/" className="text-white hover:underline">Головна</a></li>
+            <li><a href="https://techprokat.rv.ua/pro-nas/" className="text-white hover:underline">Про нас</a></li>
+            <li><a href="https://techprokat.rv.ua/prokat/" className="text-white hover:underline">Прокат</a></li>
+            <li><a href="https://techprokat.rv.ua/posluhy/" className="text-white hover:underline">Послуги</a></li>
           </ul>
         </div>
   );
@@ -40,13 +53,14 @@ const CategoriesMenu = () => {
 
 export const Footer = () => {
   return (
-    <footer className="bg-gray-100 mt-auto ">
-      <div className="max-w-[1140px] mx-auto px-4 flex flex-col md:flex-row justify-between space-y-4 md:space-y-0 py-8">
+    <footer className="bg-[#003152] opacity-100 mt-auto ">
+      <div className="max-w-[1140px] text-white mx-auto px-4 flex flex-col md:flex-row justify-between space-y-4 md:space-y-0 py-8">
         <Contacts />
         <Logo />
         <CategoriesMenu />
+        <LogoGraphicRoboty /> 
       </div>
-      <div className="text-center border-2 border-blue-700 text-gray-500 text-sm py-4 border-t">
+      <div className="text-center text-gray-500 text-sm py-4">
         &copy; {new Date().getFullYear()} TechProkat. Всі права захищені.
       </div>
     </footer>
