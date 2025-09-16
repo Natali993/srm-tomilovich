c410ab9f0ba28beeed2cbd56b5a41c1b70376632
Natali993 <manat3017@gmail.com>
2025-09-16 19:38:21 +0300

    hot-fix: mobile menu

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -29,6 +29,20 @@ const NavigationMenu = () => {
     </nav>
   );
 };
+const NavigationMenuMobile = () => {
+  return (
+    <nav>
+      <ul className="flex flex-col items-center space-y-4 text-gray-800">
+        <li><Link href="/"><p className="hover:text-blue-600">Головна</p></Link></li>
+        <li><Link href="/about"><p className="hover:text-blue-600">Про нас</p></Link></li>
+        <li><Link href="/prokat"><p className="hover:text-blue-600">Прокат</p></Link></li>
+        <li><Link href="/posluhy"><p className="hover:text-blue-600">Послуги</p></Link></li>
+        <li><Link href="/remont-honda"><p className="hover:text-blue-600">Ремонт Honda GX</p></Link></li>
+        <li><Link href="/contact"><p className="hover:text-blue-600">Контакти</p></Link></li>
+      </ul>
+    </nav>
+  );
+};
 
 const Subheader = () => {
   return (
@@ -53,12 +67,32 @@ const MainHeader = () => {
     </div>
   );
 };
-
-export const Header = () => {
+const MainHeaderMobile = () => {
   return (
-    <header className="fixed top-0 left-0 w-full z-50">
+    <div className="bg-white shadow-md">
+      <div className="container mx-auto flex justify-between items-center max-w-[1140px] py-4 px-4">
+        <Logo />
+        <NavigationMenuMobile />
+      </div>
+    </div>
+  );
+};
+
+export const DesktopHeader = () => {
+  return (
+    <header className="fixed hidden md:block top-0 left-0 w-full z-50">
       <Subheader />
       <MainHeader />
     </header>
   );
 };
+
+export const MobileHeader = () => {
+  return (
+    <header className="fixed md:hidden top-0 left-0 w-full z-50">
+      <Subheader />
+      <MainHeaderMobile />
+    </header>         
+  );
+};
+
--- src/components/CustomLayout.tsx
+++ src/components/CustomLayout.tsx
@@ -1,10 +1,11 @@
 import { Footer } from '../app/Footer';
-import { Header } from '../app/Header';
+import { DesktopHeader, MobileHeader } from '../app/Header';
 
 export const CustomLayout = ({ children }: any) => {
   return (
     <div className="flex flex-col min-h-screen bg-white font-sans text-gray-700">
-      <Header />
+      <DesktopHeader />
+      <MobileHeader />
       <main className="flex-1 max-w-[1140px] mx-auto p-4 mt-[168px]">
         {children}
       </main>
