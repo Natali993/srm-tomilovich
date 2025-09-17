9d39baf3df148e7c9eeafb0f493e5a1371541aca
Natali993 <manat3017@gmail.com>
2025-09-18 00:10:46 +0300

    chore: page about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,9 +3,22 @@ import { CustomLayout } from "@/components/CustomLayout";
 export default function About() {
   return (
     <CustomLayout>
-      <div className="flex bg-white flex-col min-h-screen font-sans text-gray-700">
-        Про нас
+      <div className="flex flex-col w-full bg-[#003152] " >
+        <h1 className="text-2xl font-bold  text-white">Про нас</h1>
+        <div className="g-breadcrumbs flex items-center space-x-2  text-white px-4 py-2 rounded">
+          <span>
+            <a
+              href="https://techprokat.rv.ua/"
+              className="g-breadcrumbs-item text-white hover:underline"
+            >
+              Головна
+            </a>
+          </span>
+          <span className="g-breadcrumbs-separator">/</span>
+          <span className="g-breadcrumbs-item text-white">Про нас</span>
+        </div>
       </div>
+
     </CustomLayout>
   );
 }
