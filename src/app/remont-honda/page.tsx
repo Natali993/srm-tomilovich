0274f5c7ecf9ec5959dd6b47c309d388651fe881
AndryKK <andrii.korbutiak@aleannlab.com>
2025-09-19 07:08:59 +0300

    feat: add component RemontDviguniv to Honda page

--- src/app/remont-honda/page.tsx
+++ src/app/remont-honda/page.tsx
@@ -1,9 +1,10 @@
 import { CustomLayout } from "@/components/CustomLayout";
 import { Breadcrumbs } from "@/components/Breadcrumbs";
+import { RemontDviguniv } from "@/components/RemontDviguniv";
 export default function RemontHonda() {
   return (
     <CustomLayout breadcrumbs={()=><Breadcrumbs title="Ремонт Honda GX" />} >
-      <></>
+      <RemontDviguniv/>
     </CustomLayout>
   );
 }
