16cf8ade816f5c6a4357dc43897ff9b67c5e3eed
AndryKK <andrii.korbutiak@aleannlab.com>
2025-09-19 07:01:34 +0300

    fix: picture Honda

--- src/components/RemontDviguniv.tsx
+++ src/components/RemontDviguniv.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { CustomTitle } from './Title';
+import Image from 'next/image';
 
 export const RemontDviguniv = () => {
 
@@ -10,16 +11,15 @@ export const RemontDviguniv = () => {
       <div className="flex flex-col md:flex-row items-start gap-8">
         {/* Left column - image and small text */}
         <div className="w-full md:w-1/2 flex flex-col items-center">
-          <img
+          <Image
             loading="lazy"
             decoding="async"
-            width="300"
-            height="300"
-            src="remontdvig.png"
-            className="w-full max-w-[300px] h-auto rounded-lg"
+            src="/remontdvig.png"
             alt="Ремонт двигунів HONDA GX"
-            srcSet="remontdvig.png 300w, remontdvig.png-150x150.png 150w, remontdvig.png-350x350.png 350w, remontdvig.png-600x600.png 600w, remontdvig.png 699w"
-            sizes="(max-width: 300px) 100vw, 300px"
+            width={300}
+            height={300}
+            layout="responsive"
+            className="w-full max-w-[300px] h-auto rounded-lg"
           />
           {/* Replaced empty spans with padding */}
           <div className="pt-2"></div>
