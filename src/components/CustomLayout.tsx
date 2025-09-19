3438e56e8692c931dc9c1f63f741f605e191dfa7
Natali993 <manat3017@gmail.com>
2025-09-19 17:48:53 +0300

    fix: main page layout

--- src/components/CustomLayout.tsx
+++ src/components/CustomLayout.tsx
@@ -7,7 +7,7 @@ export const CustomLayout = ({ children, breadcrumbs }: any) => {
     <div className="flex flex-col min-h-screen bg-white font-sans text-gray-700">
       <DesktopHeader />
       <MobileHeader />
-      <div className="mt-[164px]">{breadcrumbs &&  <Breadcrumbs />}</div>
+      {breadcrumbs && <div className="mt-[164px]"> <Breadcrumbs /></div>}
       <main className={`flex-1 max-w-[1140px] w-full mx-auto pb-4 ${breadcrumbs ? 'mt-8' : 'mt-[164px]'}`}>
         {children}
       </main>
